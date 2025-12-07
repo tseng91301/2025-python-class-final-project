7e8d33a298085f79bac2e1b7cd097e228329b3e4
tseng91301 <tseng91301@gmail.com>
2025-12-08 00:10:20 +0800

    完成初步的 api 程式建構 寫一個測試的 api 呼叫程式

--- main.py
+++
@@ -1,9 +0,0 @@
-from scripts.logs import Logs
-from scripts.genai import GenAI
-from pathlib import Path
-
-project_root = Path(__file__).parent
-
-logs = Logs(project_root / "logs")
-
-logs.write("ABC")
